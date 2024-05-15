3e3af089f35811386e1b827e0bee41761da30687
Barrett Williamson <williamsonbarrett@gmail.com>
2024-05-15 18:01:16 -0400

    fix: dont remove konsole

--- build_files/base/aurora-changes.sh
+++ build_files/base/aurora-changes.sh
@@ -15,6 +15,5 @@ if [[ "${BASE_IMAGE_NAME}" = "kinoite" ]]; then
     cp /usr/share/applications/org.gnome.Ptyxis.desktop /usr/share/kglobalaccel/org.gnome.Ptyxis.desktop
     sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.kde.konsole.desktop
     rm -f /usr/etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
-    rm -f /usr/share/kglobalaccel/org.kde.konsole.desktop
     systemctl enable kde-sysmonitor-workaround.service
 fi
